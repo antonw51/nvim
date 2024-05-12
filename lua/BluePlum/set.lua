73a286252096a8e39cb37e6c8fa0b390bfe28f7e
Anton <62518497+antonw51@users.noreply.github.com>
2024-05-12 20:22:57 +0200

    Update set.lua
    Make vim not use spaces but tabs

--- lua/BluePlum/set.lua
+++ lua/BluePlum/set.lua
@@ -4,9 +4,8 @@ vim.opt.relativenumber = true
 
 -- Four space tabs
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.softtabstop = 0 noexpandtab
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
